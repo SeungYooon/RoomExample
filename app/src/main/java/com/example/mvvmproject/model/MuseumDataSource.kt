507a6c94b554dadb0aebdbaefcebe09ca1516b49
SeungYooon <tmddbs4998@naver.com>
2020-06-30 20:46:39 +0900

    remove unuse class

--- app/src/main/java/com/example/mvvmproject/model/MuseumDataSource.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mvvmproject.model
-
-import com.example.mvvmproject.network.OperationCallback
-
-interface MuseumDataSource {
-
-    fun retrieveMuseums(callback: OperationCallback<Museum>)
-    fun cancel()
-}
--- app/src/main/java/com/example/mvvmproject/network/ServicesApiInterface.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mvvmproject.network
-
-import retrofit2.Call
-import retrofit2.http.GET
-
-interface ServicesApiInterface {
-    @GET("/api/museums/")
-    fun museums(): Call<MuseumResponse>
-}
